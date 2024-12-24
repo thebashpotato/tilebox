71129f963506f389a71a4753521aabce16b4cc5e
Matt Williams <matt.k.williams@protonmail.com>
2024-12-23 19:50:15 -0800

    Added Xephyr embed script for interactively testing the window manager

---
+++ scripts/embed.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BUILD_DIR=$(pwd)/out/build/unixlike-gcc-debug/twm
+BINARY=twm
+
+echo "Embedding ${BUILD_DIR}/${BINARY}"
+
+if [ ! -f "${BUILD_DIR}"/"${BINARY}" ]; then
+    echo "Binary not found: Please build ${BINARY} first"
+    exit 0
+fi
+
+if ! command -v Xephyr 1>/dev/null 2>&1; then
+    echo "Program not found: Install Xephyr"
+    exit 0
+fi
+
+echo "Press Ctrl+Shift to grab mouse and keyboard events in the Xephyr window, and Ctrl+Shift again to release"
+echo
+
+Xephyr :1 -ac -br -noreset -screen 1000x600 &
+XEPHYR_PID=$!
+sleep 1
+DISPLAY=:1 "${BUILD_DIR}"/${BINARY} &
+wait $XEPHYR_PID
